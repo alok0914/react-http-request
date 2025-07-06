ae7806bbdbe448e46b13b0bea58a5d921fc6f6f8
Alok Tripathi <alok_tripathi@epam.com>
2025-07-06 10:23:47 +0530

    Post user

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import UserForm from './Components/UserForm';
 import './App.css';
 import UserDetails from './Components/UserDetails';
 
 
 function App() {
-  let[showForm, setShowForm] = useState(false);
+  let [showForm, setShowForm] = useState(false);
 
-  function addUserHandler(){
+  function addUserHandler() {
     setShowForm(true);
   }
 
-  function closeForm(){
+  function closeForm() {
     setShowForm(false)
   }
 
+  function onCreateUser(user) {
+    fetch(
+      'https://react-http-tutorial-5cf12-default-rtdb.firebaseio.com/users.json', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((user) => {
+        console.log(user)
+      })
+
+  }
+
   return (
-      <div>
-        <div className='page-header'>
-          <button className='btn btn-success' onClick={addUserHandler}>Add User</button>
-          <button className='btn btn-normal'>Get Users</button>
-        </div>
-        <UserDetails></UserDetails>
-        {showForm && <UserForm closeForm={closeForm}></UserForm>}
+    <div>
+      <div className='page-header'>
+        <button className='btn btn-success' onClick={addUserHandler}>Add User</button>
+        <button className='btn btn-normal'>Get Users</button>
       </div>
+      <UserDetails></UserDetails>
+      {showForm && <UserForm closeForm={closeForm} onCreateUser={onCreateUser}></UserForm>}
+    </div>
   );
 }
 
--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './UserForm.css';
 
 function UserForm(props){
+    let fnameRef = useRef('');
+    let lnameRef = useRef('');
+    let emailRef = useRef('');
+    let passwordRef = useRef('');
+    let countryRef = useRef('India');
+    let cityRef = useRef('Delhi');
+    let dobRef = useRef('');
+    let genderRef = useRef('Male');
+
+    function onCreateUser(event) {
+        event.preventDefault();
+        let user = {
+            firstName: fnameRef.current.value,
+            lastName: lnameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            country: countryRef.current.value,
+            city: cityRef.current.value,
+            dob: dobRef.current.value,
+            gender: genderRef.current.value,
+        }
+        props.onCreateUser(user);
+    };
+
     return <>
-            <div id="myModal" class="modal">
-                    <div class="modal-content">
-                        <div class="close" onClick={props.closeForm}>&times;</div>
+            <div id="myModal" className="modal">
+                    <div className="modal-content">
+                        <div className="close" onClick={props.closeForm}>&times;</div>
                         <h3>Create new user</h3>
-                        <div class="user-form">
-                            <form>
+                        <div className="user-form">
+                            <form onSubmit={onCreateUser}>
                                 <div>
-                                    <input type="text" placeholder="First name" />
-                                    <input type="text" placeholder="Last name" />
+                                    <input type="text" placeholder="First name" ref={fnameRef} />
+                                    <input type="text" placeholder="Last name" ref={lnameRef}/>
                                 </div>
                                 <div>
-                                    <input type="email" placeholder="Email" />
+                                    <input type="email" placeholder="Email" ref={emailRef}/>
                                 </div>
                                 <div>
-                                    <input type="password" placeholder="Password" />
+                                    <input type="password" placeholder="Password" ref={passwordRef}/>
                                     <input type="password" placeholder="Confirm Password" />
                                 </div>
                                 <div>
-                                    <select name="country">
+                                    <select name="country" ref={countryRef}>
                                         <option value="India">India</option>
                                         <option value="Germany">Germany</option>
                                         <option value="USA">USA</option>
                                         <option value="UK">UK</option>
                                     </select>
-                                    <select name="city">
+                                    <select name="city" ref={cityRef}>
                                         <option value="Delhi">Delhi</option>
                                         <option value="Berlin">Berlin</option>
                                         <option value="New York">New York</option>
@@ -35,8 +59,8 @@ function UserForm(props){
                                     </select>
                                 </div>
                                 <div>
-                                    <input type="date" placeholder="Date of Birth" />
-                                    <select name="gender">
+                                    <input type="date" placeholder="Date of Birth" ref={dobRef}/>
+                                    <select name="gender" ref={ genderRef}>
                                         <option value="Male">Male</option>
                                         <option value="Female">Female</option>
                                         <option value="Unknown">Unknown</option>
